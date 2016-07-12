9055100d1ecc6e234f2d3000745a18d0e795486d
lokeshh <lokeshhsharma@gmail.com>
2016-07-12 20:01:48 +0000

    add tests for without intercept

--- spec/regression_spec.rb
+++ spec/regression_spec.rb
@@ -27,68 +27,136 @@ describe Statsample::GLM::Regression do
   # end
 
   context '#df_for_regression' do
-    context 'no interaction' do
-      include_context "formula checker", 'y~a+e' => %w[a e_B e_C y]
+    context 'with intercept' do
+      context 'no interaction' do
+        include_context "formula checker", 'y~a+e' => %w[a e_B e_C y]
+      end
+  
+      context '2-way interaction' do
+        context 'interaction of numerical with numerical' do
+          context 'none reoccur' do
+            include_context 'formula checker', 'y~a:b' =>
+              %w[a:b y]
+            end
+          
+          context 'one reoccur' do
+            include_context 'formula checker', 'y~a+a:b' =>
+              %w[a a:b y]
+          end
+  
+          context 'both reoccur' do
+            include_context 'formula checker', 'y~a+b+a:b' =>
+              %w[a a:b b y]
+          end        
+        end
+    
+        context 'interaction of category with numerical' do
+          context 'none reoccur' do
+            include_context 'formula checker', 'y~a:e' =>
+              %w[e_A:a e_B:a e_C:a y]
+          end
+  
+          context 'one reoccur' do
+            context 'numeric occur' do
+              include_context 'formula checker', 'y~a+a:e' =>
+                %w[a e_B:a e_C:a y]
+            end
+  
+            context 'category occur' do
+              include_context 'formula checker', 'y~e+a:e' =>
+                %w[e_B e_C e_A:a e_B:a e_C:a y]
+            end  
+          end        
+          
+          context 'both reoccur' do
+            include_context 'formula checker', 'y~a+e+a:e' =>
+              %w[a e_B e_C e_B:a e_C:a y]
+          end
+        end
+    
+        context 'interaction of category with category' do
+          context 'none reoccur' do
+            include_context 'formula checker', 'y~c:e' =>
+              %w[e_B e_C c_yes:e_A c_yes:e_B c_yes:e_C y]
+          end
+  
+          context 'one reoccur' do
+            include_context 'formula checker', 'y~e+c:e' =>
+              %w[e_B e_C c_yes:e_A c_yes:e_B c_yes:e_C y]
+          end
+  
+          context 'both reoccur' do
+            include_context 'formula checker', 'y~c+e+c:e' =>
+              %w[c_yes e_B e_C c_yes:e_B c_yes:e_C y]
+          end        
+        end
+      end
     end
 
-    context '2-way interaction' do
-      context 'interaction of numerical with numerical' do
-        context 'none reoccur' do
-          include_context 'formula checker', 'y~a:b' =>
-            %w[a:b y]
-          end
-        
-        context 'one reoccur' do
-          include_context 'formula checker', 'y~a+a:b' =>
-            %w[a a:b y]
-        end
-
-        context 'both reoccur' do
-          include_context 'formula checker', 'y~a+b+a:b' =>
-            %w[a a:b b y]
-        end        
+    context 'without intercept' do
+      context 'no interaction' do
+        include_context "formula checker", 'y~0+a+e' => %w[a e_A e_B e_C y]
       end
   
-      context 'interaction of category with numerical' do
-        context 'none reoccur' do
-          include_context 'formula checker', 'y~a:e' =>
-            %w[e_A:a e_B:a e_C:a y]
-        end
-
-        context 'one reoccur' do
-          context 'numeric occur' do
-            include_context 'formula checker', 'y~a+a:e' =>
-              %w[a e_B:a e_C:a y]
+      context '2-way interaction' do
+        context 'interaction of numerical with numerical' do
+          context 'none reoccur' do
+            include_context 'formula checker', 'y~0+a:b' =>
+              %w[a:b y]
+            end
+          
+          context 'one reoccur' do
+            include_context 'formula checker', 'y~0+a+a:b' =>
+              %w[a a:b y]
           end
-
-          context 'category occur' do
-            include_context 'formula checker', 'y~e+a:e' =>
-              %w[e_B e_C e_A:a e_B:a e_C:a y]
-          end  
-        end        
-        
-        context 'both reoccur' do
-          include_context 'formula checker', 'y~a+a:e' =>
-            %w[a e_B:a e_C:a y]
-        end
-      end
   
-      context 'interaction of category with category' do
-        context 'none reoccur' do
-          include_context 'formula checker', 'y~c:e' =>
-            %w[e_B e_C c_yes:e_A c_yes:e_B c_yes:e_C y]
+          context 'both reoccur' do
+            include_context 'formula checker', 'y~0+a+b+a:b' =>
+              %w[a a:b b y]
+          end        
         end
-
-        context 'one reoccur' do
-          include_context 'formula checker', 'y~e+c:e' =>
-            %w[e_B e_C c_yes:e_A c_yes:e_B c_yes:e_C y]
+    
+        context 'interaction of category with numerical' do
+          context 'none reoccur' do
+            include_context 'formula checker', 'y~0+a:e' =>
+              %w[e_A:a e_B:a e_C:a y]
+          end
+  
+          context 'one reoccur' do
+            context 'numeric occur' do
+              include_context 'formula checker', 'y~0+a+a:e' =>
+                %w[a e_B:a e_C:a y]
+            end
+  
+            context 'category occur' do
+              include_context 'formula checker', 'y~0+e+a:e' =>
+                %w[e_A e_B e_C e_A:a e_B:a e_C:a y]
+            end  
+          end        
+          
+          context 'both reoccur' do
+            include_context 'formula checker', 'y~0+a+e+a:e' =>
+              %w[a e_A e_B e_C e_B:a e_C:a y]
+          end
         end
-
-        context 'both reoccur' do
-          include_context 'formula checker', 'y~c+e+c:e' =>
-            %w[c_yes e_B e_C c_yes:e_B c_yes:e_C y]
-        end        
-      end
+    
+        context 'interaction of category with category' do
+          context 'none reoccur' do
+            include_context 'formula checker', 'y~0+c:e' =>
+              %w[c_no:e_A c_no:e_B c_no:e_C c_yes:e_A c_yes:e_B c_yes:e_C y]
+          end
+  
+          context 'one reoccur' do
+            include_context 'formula checker', 'y~0+e+c:e' =>
+              %w[e_A e_B e_C c_yes:e_A c_yes:e_B c_yes:e_C y]
+          end
+  
+          context 'both reoccur' do
+            include_context 'formula checker', 'y~0+c+e+c:e' =>
+              %w[c_yes c_no e_B e_C c_yes:e_B c_yes:e_C y]
+          end        
+        end
+      end      
     end
 
     context 'corner case' do
