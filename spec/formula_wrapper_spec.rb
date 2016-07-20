551b5a0e7b4daf86a3138dd571ed7fdf2e5197f9
lokeshh <lokeshhsharma@gmail.com>
2016-07-20 17:35:00 +0000

    update tests for * and /

--- spec/formula_wrapper_spec.rb
+++ spec/formula_wrapper_spec.rb
@@ -5,11 +5,71 @@ describe Statsample::GLM::FormulaWrapper do
   context '#preprocess_formula' do
     context 'shortcut symbols' do
       context '*' do
-        include_context 'preprocess checker', 'a*b' => 'a+b+a:b'
+        context 'two terms' do
+          include_context 'preprocess checker', 'a*b' => 'a+b+a:b'
+        end
+
+        context 'correct precedance' do
+          context 'with :' do
+            include_context 'preprocess checker', 'a*b:c' =>
+              'a+b:c+a:b:c'
+          end
+
+          context 'with +' do
+            include_context 'preprocess checker', 'a+b*c' =>
+              'a+b+c+b:c'
+          end
+        end
+
+        context 'more than two terms' do
+          include_context 'preprocess checker', 'a*b*c' =>
+            'y ~ a+b+a:b+c+a:c+b:c+a:b:c'
+        end
       end
 
       context '/' do
-        include_context 'preprocess checker', 'a/b' => 'a+a:b'
+        context 'two terms' do
+          include_context 'preprocess checker', 'a/b' => 'a+a:b'
+        end
+
+        context 'more than two terms' do
+          include_context 'preprocess checker', 'a/b/c' =>
+            'a+a:b+a:b:c'
+        end
+
+        context 'correct precedance' do
+          context 'with :' do
+            include_context 'preprocess checker', 'a/b:c' =>
+              'a+a:b:c'
+          end
+
+          context 'with +' do
+            include_context 'preprocess checker', 'a/b+c' =>
+              'a+a:b+c'
+          end
+        end
+      end
+
+      context 'brackets' do
+        context 'with + and :' do
+          include_context 'preprocess checker', '(a+b):c' =>
+            'a:c+b:c'
+        end
+
+        context 'with * and :' do
+          include_context 'preprocess checker', '(a*b):c' =>
+            'a:c+b:c+a:b:c'
+        end
+
+        context 'with / and :' do
+          include_context 'preprocess checker', '(a/b):c' =>
+            'a:c+a:b:c'
+        end
+
+        context 'with * and /' do
+          include_context 'preprocess checker' '(a*b)/c' =>
+            'a+b+a:b+a:b:c'
+        end
       end
     end
   end
